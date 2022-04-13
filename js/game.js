046e2e65f6892081fbdd2281fbcb3f357786de35
marshall-demars <marshall-demars@users.noreply.github.com>
2022-04-13 17:07:50 +0000

    Prettified Code!

--- js/game.js
+++ js/game.js
@@ -7,12 +7,12 @@
 // This file contains the JS functions for index.html
 
 const config = {
-    type: Phaser.AUTO,
-    width: 1920,
-    height: 1080,
-    
-    backgroundColor: 0x5f6e7a
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+
+  backgroundColor: 0x5f6e7a,
 }
 
-    const game = new Phaser.Game(config)
-    console.log(game)
+const game = new Phaser.Game(config)
+console.log(game)
