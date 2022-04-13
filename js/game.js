b59ce7d8e82cebe6dbcbf2cb40c3c45ec738ed80
Marshall Demars <99354910+marshall-demars@users.noreply.github.com>
2022-04-13 13:05:47 -0400

    init

---
+++ js/game.js
@@ -0,0 +1,18 @@
+/* global Phaser */
+
+// Copyright (c) 2022 Marshall All rights reserved
+//
+// Created by: Marshall
+// Created on: Apr 2022
+// This file contains the JS functions for index.html
+
+const config = {
+    type: Phaser.AUTO,
+    width: 1920,
+    height: 1080,
+    
+    backgroundColor: 0x5f6e7a
+}
+
+    const game = new Phaser.Game(config)
+    console.log(game)
