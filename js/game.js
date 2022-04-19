20ae848fb56cc7b39de6324ebb6ad2f995e59710
Marshall Demars <99354910+marshall-demars@users.noreply.github.com>
2022-04-19 16:49:52 +0000

    added comments to our files

--- js/game.js
+++ js/game.js
@@ -6,12 +6,24 @@
 // Created on: Apr 2022
 // This file contains the JS functions for index.html
 
+//* Game Scene */
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
-
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true
+    }
+  },
+  // set backround color  
   backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.Fit,
+    // we place it in the middle of the page
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
 }
 
 const game = new Phaser.Game(config)
