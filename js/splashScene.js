b05a1988989ffa78e06c7d2bc92de9d40ace2282
Marshall Demars <99354910+marshall-demars@users.noreply.github.com>
2022-04-23 23:49:03 +0000

    2 files

--- js/splashScene.js
+++ js/splashScene.js
@@ -6,22 +6,44 @@
 // Created on: Apr 2022
 // This file contains the JS functions for index.html
 
+/**
+ * This class is the Splash Scene.
+ */
 class SplashScene extends Phaser.Scene {
+  /**
+   * This method is the construtor.
+   */
   constructor() {
     super({ key: "splashScene" })
   }
 
+  /**
+   * This method is the init.
+   */
   init(data) {
-    this.camera.main.setBackroundColor("#ffffff")
+    this.cameras.main.setBackgroundColor("ffffff")
   }
 
+  /**
+   * This method is the preload.
+   */
   preload() {
     console.log("Splash Scene")
   }
 
-  create(data) {}
+  /**
+   * This method is the create.
+   */
+  create(data) {
+    // pass
+  }
 
-  update(time, delta) {}
+  /**
+   * This method is the update.
+   */
+  update(time, delta) {
+    // pass
+  }
 }
 
 export default SplashScene
