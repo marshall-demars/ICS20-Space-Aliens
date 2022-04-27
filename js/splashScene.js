2d0c33818fffe3a86312cfae23f7d1686f3c6b18
marshall-demars <marshall-demars@users.noreply.github.com>
2022-04-27 17:24:39 +0000

    Prettified Code!

--- js/splashScene.js
+++ js/splashScene.js
@@ -29,14 +29,18 @@ class SplashScene extends Phaser.Scene {
    */
   preload() {
     console.log("Splash Scene")
-    this.load.image('splashSceneBackground', './assets/splashSceneImage.png')
+    this.load.image("splashSceneBackground", "./assets/splashSceneImage.png")
   }
 
   /**
    * This method is the create.
    */
   create(data) {
-    this.splashSceneBackgroundImage = this.add.sprite(0, 0, 'splashSceneBackground')
+    this.splashSceneBackgroundImage = this.add.sprite(
+      0,
+      0,
+      "splashSceneBackground"
+    )
     this.splashSceneBackgroundImage.x = 1920 / 2
     this.splashSceneBackgroundImage.y = 1080 / 2
   }
