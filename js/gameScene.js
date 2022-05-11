cd88d4f6d0dc38adfaeea8738b64871b9bf9d5be
Marshall Demars <99354910+marshall-demars@users.noreply.github.com>
2022-05-11 12:42:54 -0400

    Update gameScene.js

--- js/gameScene.js
+++ js/gameScene.js
@@ -18,6 +18,7 @@ class GameScene extends Phaser.Scene {
 
     this.background = null
     this.ship = null
+    this.firMissile = false
   }
 
   /**
@@ -36,6 +37,7 @@ class GameScene extends Phaser.Scene {
     // pass
     this.load.image("starBackground", "assets/starBackground.png")
     this.load.image("ship", "assets/spaceShip.png")
+    this.load.image('missile', 'assets/missile.png')
   }
 
   /**
@@ -46,6 +48,9 @@ class GameScene extends Phaser.Scene {
     this.background.setOrigin(0, 0)
 
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship")
+
+      // create a group for the missiles
+    this.missileGroup = this.physics.add.group()
   }
 
   /**
@@ -53,13 +58,14 @@ class GameScene extends Phaser.Scene {
    */
   update(time, delta) {
     // called 60 times a second, hopefully
-
-    const keyLeftObj = this.input.keyboard.addKey("LEFT")
-    const keyRightObj = this.input.keyboard.addKey("RIGHT")
+      
+    const keyLeftObj = this.input.keyboard.addKey('LEFT')
+    const keyRightObj = this.input.keyboard.addKey('RIGHT')
+    const keySpaceObj = this.input.keyboard.addKey('SPACE')
 
     if (keyLeftObj.isDown === true) {
       this.ship.x -= 15
-      if (this.ship.x < 0) {
+      if (this.ship.x < 0){
         this.ship.x = 0
       }
     }
@@ -69,6 +75,19 @@ class GameScene extends Phaser.Scene {
       if (this.ship.x > 1920) {
         this.ship.x = 1920
       }
+    } 
+
+    if (keySpaceObj.isDown === true) {
+      if (this.fireMissile === false) {
+        // fire missile
+        this.fireMissile = true
+        const aNewMissile = this.physics.add.sprite(this.ship.x, this.ship.y, 'missile')
+      this.missileGroup.add(aNewMissile)
+      }
+    } 
+
+    if (keySpaceObj.isUp === true) {
+      this.fireMissile = false
     }
   }
 }
