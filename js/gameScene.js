e770d5a694c5ceeac69b86e13396461a555c0f73
marshall-demars <marshall-demars@users.noreply.github.com>
2022-05-11 16:47:44 +0000

    Prettified Code!

--- js/gameScene.js
+++ js/gameScene.js
@@ -37,7 +37,7 @@ class GameScene extends Phaser.Scene {
     // pass
     this.load.image("starBackground", "assets/starBackground.png")
     this.load.image("ship", "assets/spaceShip.png")
-    this.load.image('missile', 'assets/missile.png')
+    this.load.image("missile", "assets/missile.png")
   }
 
   /**
@@ -49,7 +49,7 @@ class GameScene extends Phaser.Scene {
 
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship")
 
-      // create a group for the missiles
+    // create a group for the missiles
     this.missileGroup = this.physics.add.group()
   }
 
@@ -58,14 +58,14 @@ class GameScene extends Phaser.Scene {
    */
   update(time, delta) {
     // called 60 times a second, hopefully
-      
-    const keyLeftObj = this.input.keyboard.addKey('LEFT')
-    const keyRightObj = this.input.keyboard.addKey('RIGHT')
-    const keySpaceObj = this.input.keyboard.addKey('SPACE')
+
+    const keyLeftObj = this.input.keyboard.addKey("LEFT")
+    const keyRightObj = this.input.keyboard.addKey("RIGHT")
+    const keySpaceObj = this.input.keyboard.addKey("SPACE")
 
     if (keyLeftObj.isDown === true) {
       this.ship.x -= 15
-      if (this.ship.x < 0){
+      if (this.ship.x < 0) {
         this.ship.x = 0
       }
     }
@@ -75,16 +75,20 @@ class GameScene extends Phaser.Scene {
       if (this.ship.x > 1920) {
         this.ship.x = 1920
       }
-    } 
+    }
 
     if (keySpaceObj.isDown === true) {
       if (this.fireMissile === false) {
         // fire missile
         this.fireMissile = true
-        const aNewMissile = this.physics.add.sprite(this.ship.x, this.ship.y, 'missile')
-      this.missileGroup.add(aNewMissile)
+        const aNewMissile = this.physics.add.sprite(
+          this.ship.x,
+          this.ship.y,
+          "missile"
+        )
+        this.missileGroup.add(aNewMissile)
       }
-    } 
+    }
 
     if (keySpaceObj.isUp === true) {
       this.fireMissile = false
@@ -92,4 +96,4 @@ class GameScene extends Phaser.Scene {
   }
 }
 
-export default GameScene 
+export default GameScene
