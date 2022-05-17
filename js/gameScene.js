d8b94d2484747bb96db3236356fd36a10b13a3fd
Marshall Demars <99354910+marshall-demars@users.noreply.github.com>
2022-05-17 16:36:14 +0000

    gameScene

--- js/gameScene.js
+++ js/gameScene.js
@@ -13,6 +13,18 @@ class GameScene extends Phaser.Scene {
   /**
    * This method is the construtor.
    */
+
+  //create an alien
+  createAlien () {
+    const alienXLocation = Math.floor(Math.random() * 1920) + 1 // this will get a number between 1 and 1920
+    let alienXVelocity = Math.floor(Math.random() * 50) + 1 // this will get a number between 1 and 50
+    alienXVelocity *= Math.round(Math.random()) ? 1 : -1 //this will add minues sign in 50% cases
+    const anAlien = this.physics.add.sprite(alienXLocation, -100, 'alien')
+    anAlien.body.velocity.y = 200
+    anAlien.body.velocity.x = alienXVelocity
+    this.alienGroup.add(anAlien)
+  }
+  
   constructor() {
     super({ key: "gameScene" })
 
@@ -38,7 +50,7 @@ class GameScene extends Phaser.Scene {
     this.load.image("starBackground", "assets/starBackground.png")
     this.load.image("ship", "assets/spaceShip.png")
     this.load.image("missile", "assets/missile.png")
-    
+    this.load.image("alien", "assets/alien.png")
     // sound
     this.load.audio("laser", "assets/laser1.wav")
   }
@@ -54,6 +66,10 @@ class GameScene extends Phaser.Scene {
 
     // create a group for the missiles
     this.missileGroup = this.physics.add.group()
+
+    // create a group for the alien
+    this.alienGroup = this.add.group()
+    this.createAlien()
   }
 
   /**
