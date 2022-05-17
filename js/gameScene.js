fddddce7032c94a6177b074a3b629a3e201afa66
Marshall Demars <99354910+marshall-demars@users.noreply.github.com>
2022-05-17 16:22:34 +0000

    init

--- js/gameScene.js
+++ js/gameScene.js
@@ -38,6 +38,7 @@ class GameScene extends Phaser.Scene {
     this.load.image("starBackground", "assets/starBackground.png")
     this.load.image("ship", "assets/spaceShip.png")
     this.load.image("missile", "assets/missile.png")
+    
     // sound
     this.load.audio("laser", "assets/laser1.wav")
   }
