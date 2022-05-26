fe4cdd394cd768fd731983c8fb710aada2b36fee
Marshall Demars <99354910+marshall-demars@users.noreply.github.com>
2022-05-26 16:28:45 +0000

    gameScene

--- js/gameScene.js
+++ js/gameScene.js
@@ -35,6 +35,9 @@ class GameScene extends Phaser.Scene {
     this.background = null
     this.ship = null
     this.firMissile = false
+    this.score = 0
+    this.scoreText = null
+    this.scoreTextStyle = { font: '65px Arial', fill: '#ffffff', align 'center' }
   }
 
   /**
@@ -67,6 +70,8 @@ class GameScene extends Phaser.Scene {
     this.background = this.add.image(0, 0, "starBackground").setScale(2.0)
     this.background.setOrigin(0, 0)
 
+    this.scoreText = this.add.text(10, 10, 'Score: ' + this.score.toString(), this.scoreTextStyle)
+
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship")
 
     // create a group for the missiles
@@ -80,7 +85,9 @@ class GameScene extends Phaser.Scene {
     this.physics.add.collider(this.missileGroup, this.alienGroup, function (missileCollide, alienCollide) {
     alienCollide.destroy()
     missileCollide.destroy()
-      this.sound.play('explosion')
+    this.sound.play('explosion')
+    this.score = this.score + 1
+    this.scoreText.setText('Score ' + this.score.toString())
     this.createAlien()
     this.createAlien()
   }.bind(this))
